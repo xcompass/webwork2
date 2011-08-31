969d7d41597955eafcc2f7b1e3ec0b58b0638b18
John Hsu <ionparticle@gmail.com>
2011-08-31 15:10:02 -0700

    Ported auto update scripts from WebworkVista.
    WebworkVista had scripts for reading the loginlist file and then automatically
    updating the courses listed there.

---
+++ lib/WebworkBridge/autoupdate.pl
@@ -0,0 +1,46 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+BEGIN {
+	die "WEBWORK_ROOT not found in environment.\n"
+		unless exists $ENV{WEBWORK_ROOT};
+}
+
+use lib "$ENV{WEBWORK_ROOT}/lib";
+use WeBWorK::CourseEnvironment;
+
+# bring up a minimal course environment
+my $ce = WeBWorK::CourseEnvironment->new({
+	webwork_dir => $ENV{WEBWORK_ROOT},
+});
+
+my $loginlistdir = $ce->{bridge}{vista_loginlist};
+open FILE, $loginlistdir or die "Cannot open loginlist file! $!\n";
+
+my @lines = <FILE>;
+foreach (@lines)
+{
+	my @line = split(/\t/, $_);
+	
+	if (scalar @line != 5)
+	{
+		print "Warning, line with unexpected format, skipping '$_' \n";
+		next;
+	}
+
+	my $userid = $line[0];
+	my $lcid = $line[1];
+	my $course = $line[2];
+
+	my $cmd = "./updateclass.pl $userid $lcid $course";
+	my $ret = `$cmd\n`;
+	if ($?)
+	{
+		die "Autoupdate failed.\n";
+	}
+	print "Autoupdate done!\n";
+}
+
+close FILE;
